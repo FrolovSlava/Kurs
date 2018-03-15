94deac426b593ec22683138331b8f9ca39a54ba4
Slava <7566724@gmail.com>
2018-03-15 19:13:38 +0300

    .

--- user.js
+++ user.js
@@ -5,6 +5,6 @@ User.prototype.hello = function (who) {
     console.log("Hello," + who.name);
 };
 
-console.log("user подключился к серверу");
+console.log("user подключился к серверу, и он даже не пил");
 
 global.User = User;
