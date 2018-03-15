fc3072df4b871ef36e86f6a7314998f2a3badc10
FrolovSlava <7566724@gmail.com>
2018-03-15 18:53:20 +0300

    рЩРЩРЩ (#2)
    * hello
    
    * ou
    
    * 1

--- server.js
+++ server.js
@@ -1,9 +1,4 @@
-function User(name) {
-    this.name= name;
-}
-User.prototype.hello = function (who) {
-    console.log("Hello," + who.name);
-};
+var user=require('user')
 
 var vasya = new User("Вася");
 var petya = new User("Петя");
---
+++ user.js
@@ -0,0 +1,6 @@
+function User(name) {
+    this.name= name;
+}
+User.prototype.hello = function (who) {
+    console.log("Hello," + who.name);
+};
